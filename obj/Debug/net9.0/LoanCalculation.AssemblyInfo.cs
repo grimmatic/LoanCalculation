4ea6f3936b592978fb7f78f544dbd0f603ed5dce
grimmatic <sevbansaricicek@gmail.com>
2025-09-22 17:01:38 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- obj/Debug/net9.0/LoanCalculation.AssemblyInfo.cs
+++ obj/Debug/net9.0/LoanCalculation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanCalculation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+849406b90a0d73b81f5fb97966e01ac6ad9767db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d225ac76a5fb82de9d9c2ae35a0727a5e862fbce")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanCalculation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanCalculation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
